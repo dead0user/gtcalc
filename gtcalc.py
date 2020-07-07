961b2bbe7969c42129836cf86d433fbf12020779
Marcin <dead0user@gmail.com>
2020-07-07 23:47:28 +0200

    0.1
    Working keyboard shortcuts

--- gtcalc.py
+++ gtcalc.py
@@ -4,21 +4,29 @@ import gi
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
+from gi.repository import Gdk
 ''' Simple calculator with RPN'''
 
 
 stack = []
-display_locker = False
+display_locker = True
 
 
 class mainWindow(Gtk.Window):
+
+    key = Gdk.KEY_0
+
     def __init__(self):
+
         Gtk.Window.__init__(self, title="gtCalc")
 
         self.grid = Gtk.Grid()
         self.grid.set_column_spacing(5)
         self.grid.set_row_spacing(5)
         self.add(self.grid)
+
+        self.connect("key-press-event", self.on_key_press_event)
+        keyname = Gdk.keyval_name(self.key)
 
         self.display = Gtk.Entry(xalign=1, editable=False)
 
@@ -52,8 +60,11 @@ class mainWindow(Gtk.Window):
         self.nine = Gtk.Button(label="9")
         self.nine.connect("clicked", self.insert_nine)
 
-        self.double_zero = Gtk.Button(label="00")
+        self.change_sign = Gtk.Button(label="CHS")
+        self.change_sign.connect("clicked", self.do_change_sign)
+
         self.point = Gtk.Button(label=".")
+        self.point.connect("clicked", self.insert_point)
 
         self.addiction = Gtk.Button(label="+")
         self.addiction.connect("clicked", self.acting_addiction)
@@ -96,7 +107,7 @@ class mainWindow(Gtk.Window):
         self.grid.attach(self.turn_off, 4, 4, 1, 1)
 
         self.grid.attach(self.zero, 0, 5, 1, 1)
-        self.grid.attach(self.double_zero, 1, 5, 1, 1)
+        self.grid.attach(self.change_sign, 1, 5, 1, 1)
         self.grid.attach(self.point, 2, 5, 1, 1)
         self.grid.attach(self.addiction, 3, 5, 1, 1)
         self.grid.attach(self.clear_display, 4, 5, 1, 1)
@@ -106,6 +117,13 @@ class mainWindow(Gtk.Window):
         Gtk.main_quit()
 
 
+    def insert_point(self, point):
+        if self.display.get_text() != "":
+            self.display.set_text(self.display.get_text() + ".")
+        else:
+            pass
+
+
     def insert_zero(self, zero):
         global display_locker
         if display_locker == True:
@@ -113,6 +131,15 @@ class mainWindow(Gtk.Window):
             display_locker = False
         else:
             self.display.set_text(self.display.get_text() + "0")
+
+
+    def do_change_sign(self, change_sign):
+        global display_locker
+        if display_locker == True:
+            self.display.set_text("-")
+            display_locker = False
+        else:
+            pass
 
 
     def insert_one(self, one):
@@ -257,6 +284,54 @@ class mainWindow(Gtk.Window):
         self.display.set_text(str(score))
         display_locker = True
         print(stack)
+
+
+    def on_key_press_event(self, grid, event):
+
+        ctrl = (event.state & Gdk.ModifierType.CONTROL_MASK)
+
+        if event.keyval == Gdk.KEY_0 or event.keyval == Gdk.KEY_KP_0:
+            self.insert_zero(self)
+        elif event.keyval == Gdk.KEY_1 or event.keyval == Gdk.KEY_KP_1:
+            self.insert_one(self)
+        elif event.keyval == Gdk.KEY_2 or event.keyval == Gdk.KEY_KP_2:
+            self.insert_two(self)
+        elif event.keyval == Gdk.KEY_3 or event.keyval == Gdk.KEY_KP_3:
+            self.insert_three(self)
+        elif event.keyval == Gdk.KEY_4 or event.keyval == Gdk.KEY_KP_4:
+            self.insert_four(self)
+        elif event.keyval == Gdk.KEY_5 or event.keyval == Gdk.KEY_KP_5:
+            self.insert_five(self)
+        elif event.keyval == Gdk.KEY_6 or event.keyval == Gdk.KEY_KP_6:
+            self.insert_six(self)
+        elif event.keyval == Gdk.KEY_7 or event.keyval == Gdk.KEY_KP_7:
+            self.insert_seven(self)
+        elif event.keyval == Gdk.KEY_8 or event.keyval == Gdk.KEY_KP_8:
+            self.insert_eight(self)
+        elif event.keyval == Gdk.KEY_9 or event.keyval == Gdk.KEY_KP_9:
+            self.insert_nine(self)
+        elif event.keyval == Gdk.KEY_KP_Separator:
+            self.insert_point(self)
+        elif event.keyval == Gdk.KEY_c:
+            self.activate_clear_display(self)
+        elif event.keyval == Gdk.KEY_Return or event.keyval == Gdk.KEY_KP_Enter:
+            self.add_to_stack(self)
+        elif event.keyval == Gdk.KEY_KP_Add:
+            self.acting_addiction(self)
+        elif event.keyval == Gdk.KEY_KP_Subtract:
+            self.acting_substraction(self)
+        elif event.keyval == Gdk.KEY_KP_Multiply:
+            self.acting_multiplication(self)
+        elif event.keyval == Gdk.KEY_KP_Divide:
+            self.acting_division(self)
+        elif ctrl and event.keyval == Gdk.KEY_minus:
+            self.do_change_sign(self)
+        elif ctrl and event.keyval == Gdk.KEY_q:
+            self.on_close_clicked(self)
+        elif ctrl and event.keyval == Gdk.KEY_d:
+            self.activate_clear_display(self)
+        else:
+            pass
 
 
 if __name__ == '__main__':
