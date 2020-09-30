c60d61b53fc72ef3f1d357e8cd26f000331f5df5
Marcin <dead0user@gmail.com>
2020-09-30 09:13:25 +0200

    0.0.2
    Działają podstawowe operacje i ich obsługa z klawiatury

--- gtcalc.py
+++ gtcalc.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
-
 import gi
-
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 from gi.repository import Gdk
+from gi.repository import Pango
+import math
 ''' Simple calculator with RPN'''
 
 
@@ -25,10 +25,14 @@ class mainWindow(Gtk.Window):
         self.grid.set_row_spacing(5)
         self.add(self.grid)
 
+        self.connect("key-press-event", self.numeric_on_key_press_event)
         self.connect("key-press-event", self.on_key_press_event)
-        keyname = Gdk.keyval_name(self.key)
 
         self.display = Gtk.Entry(xalign=1, editable=False)
+        self.display.set_max_length(25)
+        self.display.modify_font(Pango.FontDescription('Dejavu Sans Mono 11'))
+
+        self.register_display = Gtk.Entry(xalign=1, editable=False)
 
         self.zero = Gtk.Button(label="0")
         self.zero.connect("clicked", self.insert_zero)
@@ -87,13 +91,25 @@ class mainWindow(Gtk.Window):
         self.clear_display = Gtk.Button(label="CD")
         self.clear_display.connect("clicked", self.activate_clear_display)
 
+        self.square = Gtk.Button(label="SQR")
+        self.square_root = Gtk.Button(label="SQT")
+
+        self.pi_const = Gtk.Button(label="PI")
+        self.pi_const.connect("clicked", self.insert_pi_const)
+
+        self.e_const = Gtk.Button(label="E")
+        self.e_const.connect("clicked", self.insert_e_const)
+
         self.grid.attach(self.display, 0, 0, 5, 1)
+        self.grid.attach(self.register_display, 4, 0, 3, 2)
 
         self.grid.attach(self.seven, 0, 2, 1, 1)
         self.grid.attach(self.eight, 1, 2, 1, 1)
         self.grid.attach(self.nine, 2, 2, 1, 1)
         self.grid.attach(self.division, 3, 2, 1, 1)
         self.grid.attach(self.enter, 4, 2, 1, 2)
+        self.grid.attach(self.square, 5, 2, 1, 1)
+        self.grid.attach(self.square_root, 6, 2, 1, 1)
 
         self.grid.attach(self.four, 0, 3, 1, 1)
         self.grid.attach(self.five, 1, 3, 1, 1)
@@ -111,11 +127,11 @@ class mainWindow(Gtk.Window):
         self.grid.attach(self.point, 2, 5, 1, 1)
         self.grid.attach(self.addiction, 3, 5, 1, 1)
         self.grid.attach(self.clear_display, 4, 5, 1, 1)
-
+        self.grid.attach(self.pi_const, 5, 5, 1, 1)
+        self.grid.attach(self.e_const, 6, 5, 1, 1)
 
     def on_close_clicked(self, turn_off):
         Gtk.main_quit()
-
 
     def insert_point(self, point):
         if self.display.get_text() != "":
@@ -123,116 +139,116 @@ class mainWindow(Gtk.Window):
         else:
             pass
 
-
     def insert_zero(self, zero):
         global display_locker
-        if display_locker == True:
+        if display_locker is True:
             self.display.set_text("0")
             display_locker = False
         else:
             self.display.set_text(self.display.get_text() + "0")
 
-
     def do_change_sign(self, change_sign):
         global display_locker
-        if display_locker == True:
+        if display_locker is True:
             self.display.set_text("-")
             display_locker = False
         else:
             pass
 
-
     def insert_one(self, one):
         global display_locker
-        if display_locker == True:
+        if display_locker is True:
             self.display.set_text("1")
             display_locker = False
         else:
             self.display.set_text(self.display.get_text() + "1")
 
-
     def insert_two(self, two):
         global display_locker
-        if display_locker == True:
+        if display_locker is True:
             self.display.set_text("2")
             display_locker = False
         else:
             self.display.set_text(self.display.get_text() + "2")
 
-
     def insert_three(self, three):
         global display_locker
-        if display_locker == True:
+        if display_locker is True:
             self.display.set_text("3")
             display_locker = False
         else:
             self.display.set_text(self.display.get_text() + "3")
 
-
     def insert_four(self, four):
         global display_locker
-        if display_locker == True:
+        if display_locker is True:
             self.display.set_text("4")
             display_locker = False
         else:
             self.display.set_text(self.display.get_text() + "4")
 
-
     def insert_five(self, five):
         global display_locker
-        if display_locker == True:
+        if display_locker is True:
             self.display.set_text("5")
             display_locker = False
         else:
             self.display.set_text(self.display.get_text() + "5")
 
-
     def insert_six(self, six):
         global display_locker
-        if display_locker == True:
+        if display_locker is True:
             self.display.set_text("6")
             display_locker = False
         else:
             self.display.set_text(self.display.get_text() + "6")
 
-
     def insert_seven(self, seven):
         global display_locker
-        if display_locker == True:
+        if display_locker is True:
             self.display.set_text("7")
             display_locker = False
         else:
             self.display.set_text(self.display.get_text() + "7")
 
-
     def insert_eight(self, eight):
         global display_locker
-        if display_locker == True:
+        if display_locker is True:
             self.display.set_text("8")
             display_locker = False
         else:
             self.display.set_text(self.display.get_text() + "8")
 
-
     def insert_nine(self, nine):
         global display_locker
-        if display_locker == True:
+        if display_locker is True:
             self.display.set_text("9")
             display_locker = False
         else:
             self.display.set_text(self.display.get_text() + "9")
 
+    def insert_pi_const(self, pi_const):
+        self.display.set_text(str(math.pi))
+
+    def insert_e_const(self, e_const):
+        self.display.set_text(str(math.e))
 
     def activate_clear_display(self, clear_display):
         self.display.set_text('')
-
 
     def add_to_stack(self, enter):
         global display_locker
         stack.append(float(self.display.get_text()))
         display_locker = True
+        self.view_regs()
         print(stack)
 
+    def view_regs(self):
+        self.register_x_value.set_text("X: " + str(stack[-1]))
+        if len(stack) > 1:
+            self.register_y.set_text("Y: " + str(stack[-2]))
+        if len(stack) > 2:
+            self.register_z.set_text("Z: " + str(stack[-3]))
 
     def acting_addiction(self, addiction):
         global display_locker
@@ -246,7 +262,6 @@ class mainWindow(Gtk.Window):
         display_locker = True
         print(stack)
 
-
     def acting_substraction(self, substraction):
         global display_locker
         x = stack[-1]
@@ -258,7 +273,6 @@ class mainWindow(Gtk.Window):
         self.display.set_text(str(score))
         display_locker = True
         print(stack)
-
 
     def acting_multiplication(self, multiplication):
         global display_locker
@@ -272,7 +286,6 @@ class mainWindow(Gtk.Window):
         display_locker = True
         print(stack)
 
-
     def acting_division(self, division):
         global display_locker
         x = stack[-1]
@@ -285,53 +298,70 @@ class mainWindow(Gtk.Window):
         display_locker = True
         print(stack)
 
+    def numeric_on_key_press_event(self, grid, event):
+        if event.keyval == Gdk.KEY_0 or event.keyval == Gdk.KEY_KP_0:
+            self.insert_zero(self)
+
+        if event.keyval == Gdk.KEY_1 or event.keyval == Gdk.KEY_KP_1:
+            self.insert_one(self)
+
+        if event.keyval == Gdk.KEY_2 or event.keyval == Gdk.KEY_KP_2:
+            self.insert_two(self)
+
+        if event.keyval == Gdk.KEY_3 or event.keyval == Gdk.KEY_KP_3:
+            self.insert_three(self)
+
+        if event.keyval == Gdk.KEY_4 or event.keyval == Gdk.KEY_KP_4:
+            self.insert_four(self)
+
+        if event.keyval == Gdk.KEY_5 or event.keyval == Gdk.KEY_KP_5:
+            self.insert_five(self)
+
+        if event.keyval == Gdk.KEY_6 or event.keyval == Gdk.KEY_KP_6:
+            self.insert_six(self)
+
+        if event.keyval == Gdk.KEY_7 or event.keyval == Gdk.KEY_KP_7:
+            self.insert_seven(self)
+
+        if event.keyval == Gdk.KEY_8 or event.keyval == Gdk.KEY_KP_8:
+            self.insert_eight(self)
+
+        if event.keyval == Gdk.KEY_9 or event.keyval == Gdk.KEY_KP_9:
+            self.insert_nine(self)
 
     def on_key_press_event(self, grid, event):
 
         ctrl = (event.state & Gdk.ModifierType.CONTROL_MASK)
 
-        if event.keyval == Gdk.KEY_0 or event.keyval == Gdk.KEY_KP_0:
-            self.insert_zero(self)
-        elif event.keyval == Gdk.KEY_1 or event.keyval == Gdk.KEY_KP_1:
-            self.insert_one(self)
-        elif event.keyval == Gdk.KEY_2 or event.keyval == Gdk.KEY_KP_2:
-            self.insert_two(self)
-        elif event.keyval == Gdk.KEY_3 or event.keyval == Gdk.KEY_KP_3:
-            self.insert_three(self)
-        elif event.keyval == Gdk.KEY_4 or event.keyval == Gdk.KEY_KP_4:
-            self.insert_four(self)
-        elif event.keyval == Gdk.KEY_5 or event.keyval == Gdk.KEY_KP_5:
-            self.insert_five(self)
-        elif event.keyval == Gdk.KEY_6 or event.keyval == Gdk.KEY_KP_6:
-            self.insert_six(self)
-        elif event.keyval == Gdk.KEY_7 or event.keyval == Gdk.KEY_KP_7:
-            self.insert_seven(self)
-        elif event.keyval == Gdk.KEY_8 or event.keyval == Gdk.KEY_KP_8:
-            self.insert_eight(self)
-        elif event.keyval == Gdk.KEY_9 or event.keyval == Gdk.KEY_KP_9:
-            self.insert_nine(self)
-        elif event.keyval == Gdk.KEY_KP_Separator:
+        if event.keyval == Gdk.KEY_KP_Separator:
             self.insert_point(self)
-        elif event.keyval == Gdk.KEY_c:
+
+        if event.keyval == Gdk.KEY_c:
             self.activate_clear_display(self)
-        elif event.keyval == Gdk.KEY_Return or event.keyval == Gdk.KEY_KP_Enter:
+
+        if event.keyval == Gdk.KEY_Return or event.keyval == Gdk.KEY_KP_Enter:
             self.add_to_stack(self)
-        elif event.keyval == Gdk.KEY_KP_Add:
+
+        if event.keyval == Gdk.KEY_KP_Add:
             self.acting_addiction(self)
-        elif event.keyval == Gdk.KEY_KP_Subtract:
+
+        if event.keyval == Gdk.KEY_KP_Subtract:
             self.acting_substraction(self)
-        elif event.keyval == Gdk.KEY_KP_Multiply:
+
+        if event.keyval == Gdk.KEY_KP_Multiply:
             self.acting_multiplication(self)
-        elif event.keyval == Gdk.KEY_KP_Divide:
+
+        if event.keyval == Gdk.KEY_KP_Divide:
             self.acting_division(self)
-        elif ctrl and event.keyval == Gdk.KEY_minus:
+
+        if ctrl and event.keyval == Gdk.KEY_minus:
             self.do_change_sign(self)
-        elif ctrl and event.keyval == Gdk.KEY_q:
+
+        if ctrl and event.keyval == Gdk.KEY_q:
             self.on_close_clicked(self)
-        elif ctrl and event.keyval == Gdk.KEY_d:
+
+        if ctrl and event.keyval == Gdk.KEY_d:
             self.activate_clear_display(self)
-        else:
-            pass
 
 
 if __name__ == '__main__':
